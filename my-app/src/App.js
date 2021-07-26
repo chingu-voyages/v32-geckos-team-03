b6ba9e24e8d643025d68728e8a46fbd1878e4395
Rayhan1998 <miahrayhan01@gmail.com>
2021-07-26 18:03:39 -0400

    installed axios and react router dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+      )
+      .then(res => {
+        console.log(res);
+      });
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
