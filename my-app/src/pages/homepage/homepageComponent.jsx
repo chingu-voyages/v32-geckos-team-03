c27b3f9af094ea703d7df4f37519695e21b9d05a
Rayhan1998 <miahrayhan01@gmail.com>
2021-08-05 00:00:14 -0400

    .

--- my-app/src/pages/homepage/homepageComponent.jsx
+++ my-app/src/pages/homepage/homepageComponent.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import "./homepage.styles.css";
 
 import Header from "../../components/header/header.component";
+import SelectionBox from "../../components/selectionBox/selectionBox.component";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <Header />
+      <div className="homepage-inner">
+        <Header />
+        <div className="homepage-boxes">
+          <SelectionBox title="General Knowledge Quiz" buttonName="BEGIN" />
+          <SelectionBox title="Select a Topic" buttonName="SELECT" />
+        </div>
+      </div>
     </div>
   );
 }
