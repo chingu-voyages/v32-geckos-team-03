9c709eec950482412a19e24492e797cb307da3a8
Thiago Souza <thiagorasouza@gmail.com>
2021-08-25 01:05:32 -0300

    Delete backend folder (moved to a separate repo)

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-exports.FRONTEND_ADDRESS = "http://localhost";
-exports.FRONTEND_PORT = 4000;
-exports.BACKEND_PORT = 3000;
-exports.CONNECTION_STRING =
-  "mongodb+srv://thiago:q02yBCsOfR0dI5ca@cluster0.ksvqe.mongodb.net/app?retryWrites=true&w=majority";
