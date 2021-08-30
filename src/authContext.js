e1222aee3d6ce6b677ea544ca4f06ed538df79fc
Thiago Souza <thiagorasouza@gmail.com>
2021-08-30 17:56:34 -0300

    Add axios interceptors

--- src/authContext.js
+++ src/authContext.js
@@ -7,6 +7,16 @@ class ProvideAuth extends Component {
   state = { user: undefined };
 
   componentDidMount() {
+    axios.interceptors.request.use((request) => {
+      console.log("Starting Request", JSON.stringify(request, null, 2));
+      return request;
+    });
+
+    axios.interceptors.response.use((response) => {
+      console.log("Response:", JSON.stringify(response, null, 2));
+      return response;
+    });
+
     this.setAuthContext();
   }
 
